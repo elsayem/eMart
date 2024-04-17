483e1330a6575befe2a3bfe7367302ec0d5a4e5f
asaiem <eng.abdulrahman.alsaiem@gmail.com>
2024-04-17 19:09:53 +0200

    Add Category ViewModel

---
+++ eMart.Business/ViewModels/CategoryVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMart.Business.ViewModels
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+    
+        [MinLength(3,ErrorMessage ="Name Must be more than two character !")]
+        [Required(ErrorMessage ="Category Name is required")]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+
+        [MinLength(3, ErrorMessage = "Description Must be more than two character !")]
+        public string? Description { get; set; }
+
+        public byte[]? Thumbnail { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
